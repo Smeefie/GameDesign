2813da8af2672fe44f6340d0894b04af85b2c9ac
Stefan Kaya <Stefanzulfikarkaya@hotmail.com>
2020-04-09 16:11:23 +0200

    Added Cheats to master

---
+++ Assets/Resources/Scripts/Cheat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Cheat : MonoBehaviour
+{
+    public bool Enabled = false;
+
+    void Update()
+    {
+        if (enabled && Condition())
+        {
+            Debug.Log("The cheat " + GetName() + " has been pressed.");
+            Action();
+        }
+    }
+    protected abstract void Action();
+    protected abstract bool Condition();
+    protected abstract string GetName();
+}
---
+++ Assets/Resources/Scripts/Teleport.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Teleport : Cheat
+{
+    public Transform Player;
+
+    protected override void Action()
+    {
+       Player.localPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    protected override bool Condition()
+    {
+        return Input.GetKeyDown(KeyCode.Q);
+    }
+
+    protected override string GetName()
+    {
+        return "Teleport";
+    }
+}
